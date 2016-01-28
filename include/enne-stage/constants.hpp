fa0d10af7cafa423e1c30ef284d5ead68d78b6aa
Enne Hekma <ennehekma@gmail.com>
2016-01-28 15:51:22 +0100

    regular update

---
+++ include/enne-stage/constants.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014-2016 Kartik Kumar, Dinamica Srl (me@kartikkumar.com)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+#ifndef ENNE_STAGE_CONSTANTS_HPP
+#define ENNE_STAGE_CONSTANTS_HPP
+
+namespace enne_stage
+{
+
+//! \f$\pi\f$ to 20 decimal places.
+static const double ENNE_STAGE_PI = 3.14159265358979323846;
+static const double ASTRO_ENNE_GM_EARTH = 3.98600441e14;
+static const double ASTRO_ENNE_RADIUS_EARTH = 6378136; // # meter;
+static const double kAE = 1.0;
+static const double kQ0 = 120.0;
+static const double kS0 = 78.0;
+static const double kMU = 398600.8;
+static const double kXKMPER = 6378.135;
+static const double kXJ2 = 1.082616e-3;
+static const double kXJ3 = -2.53881e-6;
+static const double kXJ4 = -1.65597e-6;
+static const double kPI = 3.14159265358979323846264338327950288419716939937510582;
+static const double kAU = 1.49597870691e8;
+
+} // namespace sml
+
+#endif // ENNE_STAGE_CONSTANTS_HPP
---
+++ include/enne-stage/factorial.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) <year> <author> (<email>)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+#ifndef CPP_PROJECT_HPP
+#define CPP_PROJECT_HPP
+
+namespace cpp_project
+{
+
+//! Compute factorial.
+/*!
+ * Computes factorial of a given integer.
+ * @param  integerNumber Integer number to compute factorial of
+ * @return               Value of factorial of integer
+ */
+const int computeFactorial( const int integerNumber );
+
+} // namespace cpp_project
+
+#endif // CPP_PROJECT_HPP
